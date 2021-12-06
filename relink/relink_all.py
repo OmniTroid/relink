1b32dce782571de0aa9b73af99332837f1bc6447
David Skoland <davidskoland@gmail.com>
2021-12-06 22:38:49 +0100

    Finish relink_all

--- relink/relink_all.py
+++ relink/relink_all.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python3
 
+import os
 import sys
+from pathlib import Path
 
 sys.path.append('.')
 
 import relink
 
+# Runs relink on all symlinks in given directory
+def relink_all(dir_ : Path):
+	if not dir_.is_dir():
+		print('Error: ' + str(dir_) + ' is not a directory.')
+		return
+
+	files = [file for file in dir_.iterdir() if file.is_symlink()]
+
+	for file in files:
+		relink.relink(file)
+
 def main():
-	print('h')
+	if len(sys.argv) < 2:
+		print('Error: expected one argument: directory')
+		exit(1)
+
+	relink_all(Path(sys.argv[1]))
+
+if __name__ == '__main__':
+	main()
